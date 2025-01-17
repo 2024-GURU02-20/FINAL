810df747b97df80e329f82e8e09b6e96efeb56ba
SaltySalt77 <hyna0707@gmail.com>
2025-01-17 12:23:11 +0900

    Chore: 뷰 바인딩 설정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.android.myapplication"
     compileSdk = 34
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.android.myapplication"
         minSdk = 24
