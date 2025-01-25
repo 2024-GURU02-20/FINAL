ca813924c274d77af5787dd5fd75241d710a002a
SaltySalt77 <hyna0707@gmail.com>
2025-01-25 21:08:47 +0900

    Feat: 검색 결과 페이지 이동 동작 구현

--- app/src/main/java/com/android/myapplication/BookListFragment.kt
+++ app/src/main/java/com/android/myapplication/BookListFragment.kt
@@ -27,9 +27,15 @@ class BookListFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         binding = FragmentBookListBinding.inflate(inflater, container, false)
-
+        // 검색창 누르면 검색 페이지로 이동
         binding.search.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
+                val searchFragment: SearchFragment = SearchFragment()
+
+                parentFragmentManager.beginTransaction()
+                    .replace(R.id.rootlayout, searchFragment)
+                    .addToBackStack(null) // 뒤로 가기 스택 추가
+                    .commit()
             }
         })
 
