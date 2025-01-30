d4acff6156fb4d3cbe85ee864fa4deea0d081a4f
hynn <kshyun019@naver.com>
2025-01-30 17:50:31 +0900

    TEST : 테스트 코드 // materialcalendarview 수정 필요

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,18 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    id("kotlin-kapt")
+    id("org.jetbrains.kotlin.kapt")
+}
+
+
+val localProperties = Properties()
+val localPropertiesFile = rootProject.file("local.properties")
+if (localPropertiesFile.exists()) {
+    localPropertiesFile.inputStream().use {
+        localProperties.load(it)
+    }
 }
 
 android {
@@ -10,10 +21,7 @@ android {
 
     buildFeatures {
         viewBinding = true
-    }
-
-    viewBinding {
-        enable = true
+        buildConfig = true
     }
 
     defaultConfig {
@@ -24,6 +32,8 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        val aladinApiKey: String = localProperties.getProperty("ALADIN_API_KEY") ?: ""
+        buildConfigField("String", "ALADIN_API_KEY", "\"$aladinApiKey\"")
     }
 
     buildTypes {
@@ -45,29 +55,30 @@ android {
 }
 
 dependencies {
+    implementation("com.github.bumptech.glide:glide:4.15.1")
+    kapt("com.github.bumptech.glide:compiler:4.15.1")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+//    implementation(libs.mediation.test.suite)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-    // Retrofit 및 OkHttp
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:okhttp:4.10.0")
-
     // Material Calendar View
     implementation("com.prolificinteractive:material-calendarview:1.4.3")
 
-    // Glide (이미지 로드)
-    implementation("com.github.bumptech.glide:glide:4.15.1")
-    kapt("com.github.bumptech.glide:compiler:4.15.1")
-
     // AndroidX Core (필요 시 추가)
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-
+//    implementation("androidx.core:core-ktx:1.10.1")
+//    implementation("androidx.appcompat:appcompat:1.6.1")
 }
--- app/src/main/java/com/android/myapplication/GlideImageDecorator.kt
+++ app/src/main/java/com/android/myapplication/GlideImageDecorator.kt
@@ -1,39 +1,39 @@
 package com.android.myapplication
 
-import android.content.Context
-import android.text.TextPaint
-import android.text.style.ImageSpan
-import android.widget.ImageView
-import com.bumptech.glide.Glide
-import com.prolificinteractive.materialcalendarview.CalendarDay
-import com.prolificinteractive.materialcalendarview.DayViewFacade
-import com.prolificinteractive.materialcalendarview.DayViewDecorator
-
-class GlideImageDecorator(
-    private val context: Context,
-    private val date: CalendarDay,
-    private val imageUrl: String
-) : DayViewDecorator {
-
-    override fun shouldDecorate(day: CalendarDay./): Boolean {
-        return day == date
-    }
-
-    override fun decorate(view: DayViewFacade) {
-        // 이미지를 Glide로 로드해서 날짜에 추가
-        val imageView = ImageView(context)
-        Glide.with(context)
-            .load(imageUrl)
-            .into(imageView)
-
-        // ImageSpan을 사용하여 이미지를 날짜에 추가
-        view.addSpan(object : Span {
-            override fun updateDrawState(paint: TextPaint?) {
-                // 이미지를 텍스트 대신 그리기 위해서 처리
-            }
-        })
-    }
-    override fun decorate(view: DayViewFacade) {
-        val imageSpan = ImageSpan(context, R.drawable.image)
-        view.addSpan(imageSpan)  // 이미지를 추가
-}
+//import android.content.Context
+//import android.text.TextPaint
+//import android.text.style.ImageSpan
+//import android.widget.ImageView
+//import com.bumptech.glide.Glide
+//import com.prolificinteractive.materialcalendarview.CalendarDay
+//import com.prolificinteractive.materialcalendarview.DayViewFacade
+//import com.prolificinteractive.materialcalendarview.DayViewDecorator
+//
+//class GlideImageDecorator(
+//    private val context: Context,
+//    private val date: CalendarDay,
+//    private val imageUrl: String
+//) : DayViewDecorator {
+//
+//    override fun shouldDecorate(day: CalendarDay./): Boolean {
+//        return day == date
+//    }
+//
+//    override fun decorate(view: DayViewFacade) {
+//        // 이미지를 Glide로 로드해서 날짜에 추가
+//        val imageView = ImageView(context)
+//        Glide.with(context)
+//            .load(imageUrl)
+//            .into(imageView)
+//
+//        // ImageSpan을 사용하여 이미지를 날짜에 추가
+//        view.addSpan(object : Span {
+//            override fun updateDrawState(paint: TextPaint?) {
+//                // 이미지를 텍스트 대신 그리기 위해서 처리
+//            }
+//        })
+//    }
+//    override fun decorate(view: DayViewFacade) {
+//        val imageSpan = ImageSpan(context, R.drawable.image)
+//        view.addSpan(imageSpan)  // 이미지를 추가
+//}
