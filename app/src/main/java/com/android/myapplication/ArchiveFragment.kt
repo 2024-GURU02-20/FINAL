93c1cf75d9b19e70fef3041ebfbf771e46645975
SaltySalt77 <hyna0707@gmail.com>
2025-02-03 20:54:15 +0900

    Fix: 빌드 안되는 에러

--- app/src/main/java/com/android/myapplication/ArchiveFragment.kt
+++ app/src/main/java/com/android/myapplication/ArchiveFragment.kt
@@ -28,65 +28,55 @@ import java.util.Locale
 class ArchiveFragment : Fragment() {
     private lateinit var binding: FragmentArchiveBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        super.onCreateView(inflater, container, savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         binding = FragmentArchiveBinding.inflate(inflater, container, false)
+
+        binding.calendarView.dayBinder = object : MonthDayBinder<DayViewContainer> {
+            override fun create(view: View) = DayViewContainer(view)
+
+            override fun bind(container: DayViewContainer, day: CalendarDay) {
+                container.dayText.text = day.date.dayOfMonth.toString()
+            }
+        }
+
+        val currentMonth = YearMonth.now()
+        val startMonth = currentMonth.minusMonths(6) // 6개월 전부터
+        val endMonth = currentMonth.plusMonths(6) // 6개월 후까지
+        val firstDayOfWeek = WeekFields.of(Locale.getDefault()).firstDayOfWeek
+
+        binding.calendarView.setup(startMonth, endMonth, firstDayOfWeek)
+        binding.calendarView.scrollToMonth(currentMonth)
+
+        binding.customProfileView.setOnClickListener {
+            val user = FirebaseAuth.getInstance().currentUser
+            if (user == null) {
+                val intent = Intent(requireContext(), LoginActivity::class.java)
+                startActivity(intent)
+            }
+
+        }
+
         return binding.root
     }
 
-    class ArchiveFragment : Fragment() {
-        private lateinit var binding: FragmentArchiveBinding
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-        }
-
-        override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-        ): View {
-            binding = FragmentArchiveBinding.inflate(inflater, container, false)
-
-            binding.calendarView.dayBinder = object : MonthDayBinder<DayViewContainer> {
-                override fun create(view: View) = DayViewContainer(view)
-
-                override fun bind(container: DayViewContainer, day: CalendarDay) {
-                    container.dayText.text = day.date.dayOfMonth.toString()
-                }
-            }
-
-            val currentMonth = YearMonth.now()
-            val startMonth = currentMonth.minusMonths(6) // 6개월 전부터
-            val endMonth = currentMonth.plusMonths(6) // 6개월 후까지
-            val firstDayOfWeek = WeekFields.of(Locale.getDefault()).firstDayOfWeek
-
-            binding.calendarView.setup(startMonth, endMonth, firstDayOfWeek)
-            binding.calendarView.scrollToMonth(currentMonth)
-
-            val user = FirebaseAuth.getInstance().currentUser
-            if (user != null) {
-                binding.customProfileView.setData( user.displayName + "님의", "독서 기록입니다", user.photoUrl)
-            }
-
-            binding.customProfileView.setOnClickListener {
-                val user = FirebaseAuth.getInstance().currentUser
-                if (user == null) {
-                    val intent = Intent(requireContext(), LoginActivity::class.java)
-                    startActivity(intent)
-                }
-
-            }
-
-            return binding.root
-        }
-
-        class DayViewContainer(view: View) : ViewContainer(view) {
-            val dayText: TextView = view.findViewById(R.id.day_text)
+        val user = FirebaseAuth.getInstance().currentUser
+        if (user != null) {
+            binding.customProfileView.setData( user.displayName + "님의", "독서 기록입니다", user.photoUrl)
         }
     }
+
+    class DayViewContainer(view: View) : ViewContainer(view) {
+        val dayText: TextView = view.findViewById(R.id.day_text)
+    }
 }
+
