84b41919a7cc610cf449df80d8e07d0cc1e29d15
hynn <kshyun019@naver.com>
2025-02-03 23:43:43 +0900

    Refactor : 코드 리팩토링 [아카이빙 메인 화면] 캘린더 성공!!!!!!!

--- app/src/main/java/com/android/myapplication/ArchiveFragment.kt
+++ app/src/main/java/com/android/myapplication/ArchiveFragment.kt
@@ -1,30 +1,26 @@
 package com.android.myapplication
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
-import com.android.myapplication.DB.AppDatabase
-import com.android.myapplication.DB.Review
-import com.android.myapplication.DB.User
 import com.android.myapplication.databinding.FragmentArchiveBinding
-import com.android.myapplication.databinding.FragmentSearchBinding
-import com.android.myapplication.databinding.FragmentSearchResultBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.kizitonwose.calendar.core.CalendarDay
+import com.kizitonwose.calendar.core.CalendarMonth
+import com.kizitonwose.calendar.core.DayPosition
 import com.kizitonwose.calendar.view.MonthDayBinder
 import com.kizitonwose.calendar.view.ViewContainer
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import java.time.YearMonth
 import java.time.temporal.WeekFields
 import java.util.Locale
+import com.kizitonwose.calendar.core.OutDateStyle
+import com.kizitonwose.calendar.view.MonthHeaderFooterBinder
+import com.kizitonwose.calendar.view.MonthScrollListener
 
 class ArchiveFragment : Fragment() {
     private lateinit var binding: FragmentArchiveBinding
@@ -43,7 +39,14 @@ class ArchiveFragment : Fragment() {
             override fun create(view: View) = DayViewContainer(view)
 
             override fun bind(container: DayViewContainer, day: CalendarDay) {
+                val currentMonth = YearMonth.now()
                 container.dayText.text = day.date.dayOfMonth.toString()
+
+                if (day.date.year == currentMonth.year && day.date.month == currentMonth.month) {
+                    container.dayText.setTextColor(Color.BLACK) // 현재 월의 날짜
+                } else {
+                    container.dayText.setTextColor(Color.GRAY) // 이전/다음 달의 날짜
+                }
             }
         }
 
@@ -52,8 +55,15 @@ class ArchiveFragment : Fragment() {
         val endMonth = currentMonth.plusMonths(6) // 6개월 후까지
         val firstDayOfWeek = WeekFields.of(Locale.getDefault()).firstDayOfWeek
 
+
         binding.calendarView.setup(startMonth, endMonth, firstDayOfWeek)
         binding.calendarView.scrollToMonth(currentMonth)
+
+        binding.calendarView.monthScrollListener = { month ->
+            val yearMonth = month.yearMonth
+            binding.monthTitle.text = "${yearMonth.year}년 ${yearMonth.monthValue}월"
+        }
+
 
         binding.customProfileView.setOnClickListener {
             val user = FirebaseAuth.getInstance().currentUser
