80c1f0d2cb7440ce06e58d748cbb18f447fa6c6a
codenameVien <ejej2003@naver.com>
2025-02-04 18:51:21 +0900

    Refactor: 주석 제거

--- app/src/main/java/com/android/myapplication/DB/ReviewDao.kt
+++ app/src/main/java/com/android/myapplication/DB/ReviewDao.kt
@@ -63,7 +63,6 @@ interface ReviewDao {
     @Query("SELECT favoriteLine FROM review WHERE isbn = :isbn LIMIT 3")
     suspend fun getFavoriteLinesByIsbn(isbn: String): List<String>
 
-    //은정 추가
     //가장 많은 리뷰를 남긴 유저를 찾고 해당 유저의 ISBN 12개를 가져옴
     //LIMIT 12 → 최대 12개만 가져오도록 설정
     @Query("""
@@ -76,9 +75,6 @@ interface ReviewDao {
     //추천 개수가 높은 순서대로 정렬하여 상위 3개의 리뷰만 가져오기
     @Query("SELECT * FROM review ORDER BY `like` DESC LIMIT 3")
     suspend fun getTopLikedReviews(): List<Review>
-//
-//    // 해당 리뷰의 추천수 1증가 (추천 버튼 클릭 시)
-//    @Query("UPDATE review SET `like` = `like` + 1 WHERE reviewId = :reviewId")
-//    suspend fun increaseReviewLike(reviewId: Int)
+
 
 }
